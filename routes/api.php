7a73a254c6ff37af12b8a72b1e8676465f6645bc
Amawers <dacoroon.christianjericho@gmail.com>
2024-04-18 00:51:32 +0800

    feat: made api for mobile

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Course;
 use App\Models\User;
 
+
 // PUBLIC ROUTES
 Route::post('/signup', [UserController::class, 'signup']);
 Route::post('/login', [UserController::class, 'login']);
@@ -13,13 +14,61 @@ Route::post('/login', [UserController::class, 'login']);
 // PRIVATE ROUTES
 Route::group(['middleware' => ['auth:sanctum']], function () {
 
-    /**
-     * Resource for authenticated users only
-     */
-    Route::get('/testing', function () {
-        return 'TEST!!!';
+    // Dashboard Data
+    Route::get('/dashboard', function () {
+        $data = User::find(Auth::id())->select('id')->first();
+        $courseDataArr = array();
+        foreach($data->courses as $course){
+            $courseId = $course->id;
+            $imagePath = $course->image_path;
+            $instructorName = $course->instructor_name;
+            $title = $course->title;
+            $progress = $course->progress;
+
+            array_push($courseDataArr, ['course_id' => $courseId, 'instructor_name' => $instructorName, 'image_path' => $imagePath, 'title' => $title, 'progress' => $progress]);
+        }
+
+        $response = [
+            'message' => 'Successfully Retrieve Dashboard Data.',
+            'data' => $courseDataArr,
+            'success' => true
+        ];
+        return response($response, 200);
     });
 
+    // Specific Course Data
+    Route::get('/course/{course_id}', function($course_id){
+        $course = Course::find($course_id);
+        $courseDetail = Course::find($course_id)->course_detail;
+        $courseObjective = Course::find($course_id)->course_objective;
+        $objectivesArr = array();
+        foreach($courseObjective as $objective){
+            array_push($objectivesArr ,$objective->objective);
+        }
+
+        $courseTopic = Course::find($course_id)->course_topic;
+        $topicsArr = array();
+        foreach($courseTopic as $topic){
+            array_push($topicsArr ,$topic->topic);
+        }
+
+        $learningMaterial = Course::find($course_id)->learning_material;
+        $materialsArr = array();
+        foreach($learningMaterial as $material){
+            array_push($materialsArr, $material->lesson_name);
+        }
+
+        $response = [
+            'message' => 'Successfully Retrieve Specific Course Data.',
+            'data' => ['instructor_name' => $course->instructor_name,  'title' => $course->title, 'description' => $courseDetail->first()->description, 'objectives' => $objectivesArr, 'topics' => $topicsArr, 'materials' => $materialsArr],
+            'success' => true
+        ];
+
+        return response($response, 200);
+    });
+
+
+    // Logout Authenticated User
     Route::delete('/logout', [UserController::class, 'logout']);
 });
 
